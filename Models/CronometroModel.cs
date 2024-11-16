863661a0832407c218dbfcc0ab5625128e69a465
eugedipaolo <eugenia.dipaolo@gmail.com>
2024-11-16 22:44:27 +0100

    Add files via upload

---
+++ Models/CronometroModel.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Threading;
+
+namespace Cronometro.Models {
+    public interface ICronometro {
+        event EventHandler SecondTick;
+        void Start();
+        void Pause();
+        void Stop();
+        string GetActualTime();
+    }
+    internal class CronometroModel:ICronometro {
+        private DispatcherTimer _timer;
+        private TimeSpan _elapsedTime;
+
+        public event EventHandler? SecondTick;
+
+        public CronometroModel() {
+            _timer = new DispatcherTimer { Interval = TimeSpan.FromSeconds(1) };
+            _timer.Tick += (sender, args) =>  {
+                _elapsedTime = _elapsedTime.Add(TimeSpan.FromSeconds(1));
+                SecondTick?.Invoke(this, EventArgs.Empty);
+            };
+            _elapsedTime = TimeSpan.Zero;
+        }
+
+        public void Start() => _timer.Start();
+        public void Pause() => _timer.Stop();
+        public void Stop() {
+            _timer.Stop();
+            _elapsedTime = TimeSpan.Zero;
+        }
+
+        public string GetActualTime() => _elapsedTime.ToString(@"hh\:mm\:ss");
+     }
+  
+}
---
+++ ViewModels/CronometroViewModel.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Cronometro.Helpers;
+using Cronometro.Models;
+using System.Windows.Input;
+
+namespace Cronometro.ViewModels {
+    public class CronometroViewModel:INotifyPropertyChanged {
+      
+            private readonly ICronometro _cronometro;
+            private string _tiempo = string.Empty;
+
+        public string Tiempo
+        {
+            get => _tiempo;
+
+            set
+            {
+                _tiempo = value;
+                OnPropertyChanged(nameof(Tiempo));
+            }
+        }
+
+        public ICommand StartCommand { get; }
+            public ICommand PauseCommand { get; }
+            public ICommand StopCommand { get; }
+            
+
+        public CronometroViewModel(ICronometro cronometro) {
+                _cronometro = cronometro;
+                _cronometro.SecondTick += (sender, e) => Tiempo = _cronometro.GetActualTime();
+                StartCommand = new RelayCommand(_ => Start());
+                PauseCommand = new RelayCommand(_ => Pause());
+                StopCommand  = new RelayCommand(_ => Stop());
+                Tiempo = _cronometro.GetActualTime() ?? string.Empty;
+        }
+
+            private void Start() => _cronometro.Start();
+            private void Pause() => _cronometro.Pause();
+            private void Stop() => _cronometro.Stop();
+
+            public event PropertyChangedEventHandler? PropertyChanged;
+            protected void OnPropertyChanged(string propertyName) => PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+
+
